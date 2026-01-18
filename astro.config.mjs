1b90a4f91932fc86779cceae2c8f835462e1d41b
Kkeryyann <keryann2000@gmail.com>
2026-01-18 18:38:50 +0100

    fix : units tests n°2.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,9 +10,5 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [react(), tailwind()],
 });
